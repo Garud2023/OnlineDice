3a67dbd0662bf4c97e9bfbcd4140c6bb7b964aad
Garud2023 <121758741+Garud2023@users.noreply.github.com>
2024-10-01 21:07:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var randomNumber = Math.floor(Math.random()*6) + 1;  // 1 to 6
 
 var randomDiceImage =  "dice" + randomNumber + ".png";  // dice1.png to dice6.png
 
-// var randomImageSource = "images/" + randomDiceImage; // ./images/dice1.png to ./images/dice6.png
 
 var image = document.querySelectorAll("img")[0];
 
